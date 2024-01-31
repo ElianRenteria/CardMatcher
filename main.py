f42b2f27cd4e67ef00f615fa7f109e1707bd1b12
Elian Renteria <elianrenteriadevelopment@gmail.com>
2024-01-30 22:13:36 -0800

    added postgame screen

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import card
 
 
 pygame.init()
-matchSound = pygame.mixer.Sound("sounds/match.wav")
-matchSound.set_volume(.75)
+matchSound = pygame.mixer.Sound("sounds/match.mp3")
+matchSound.set_volume(.5)
 flipSound = pygame.mixer.Sound("sounds/flip.wav")
 flipSound.set_volume(.5)
+game_complete_sound = pygame.mixer.Sound("sounds/game_complete.mp3")
+game_complete_sound.set_volume(.75)
+game_complete = False
+font = pygame.font.Font('freesansbold.ttf', 32)
+winner_text = font.render("Play Again", True, (35, 35, 35))
+textRect = winner_text.get_rect()
+textRect.center = (800, 500)
+play_again_button = pygame.Rect(550,450,500,100)
 cards = []
 for i in range(0,14):
     cards.append(pygame.transform.scale(pygame.image.load("images/"+str(i)+".png"),(100,145)))
@@ -15,6 +23,8 @@ for i in range(0,14):
 game = []
 match = pygame.transform.scale(pygame.image.load("match.png"),(578,262))
 def shuffle():
+    global game
+    game = []
     deck1 = cards[1:14]
     deck2 = cards[1:14]
     while len(deck1) > 0 or len(deck2) > 0:
@@ -45,7 +55,7 @@ def pickCard():
                 check(card1, card2)
 
 def check(c1,c2):
-    global card1, card2, matchSound
+    global card1, card2, matchSound, game, game_complete
     if c1.type != c2.type:
         draw()
         pygame.display.update()
@@ -57,45 +67,66 @@ def check(c1,c2):
         draw()
         window.blit(match, (511, 300))
         pygame.display.update()
-        matchSound.play(0,2000,500)
+        matchSound.play(0,2500,500)
         pygame.time.wait(1000)
     card1, card2 = None, None
+    game_complete = True
+    for card in game:
+        if not card.show:
+            game_complete = False
+            break
+    if game_complete:
+        game_complete_sound.play(0,3000,500)
+
+
 
 def draw():
-    window.fill((53, 101, 77))
-    for i in range(0, len(game)):
-        if i < 6:
-            if game[i].show:
-                window.blit(game[i].image, ((i)*150+380, 100))
+    if not game_complete:
+        window.fill((53, 101, 77))
+        for i in range(0, len(game)):
+            if i < 6:
+                if game[i].show:
+                    window.blit(game[i].image, ((i)*150+380, 100))
+                else:
+                    window.blit(cards[0], ((i) * 150 + 380, 100))
+                    game[i].x = (i) * 150 + 380
+                    game[i].y = 100
+                    game[i].hitBox = pygame.Rect(game[i].x, game[i].y, 100, 145)
+            elif i < 13:
+                if game[i].show:
+                    window.blit(game[i].image, ((i-6)*150+300, 300))
+                else:
+                    window.blit(cards[0], ((i - 6) * 150 + 300, 300))
+                    game[i].x = (i-6)*150+300
+                    game[i].y = 300
+                    game[i].hitBox = pygame.Rect(game[i].x, game[i].y, 100, 145)
+            elif i < 20:
+                if game[i].show:
+                    window.blit(game[i].image, ((i-13)*150+300, 500))
+                else:
+                    window.blit(cards[0], ((i - 13) * 150 + 300, 500))
+                    game[i].x = (i - 13) * 150 + 300
+                    game[i].y = 500
+                    game[i].hitBox = pygame.Rect(game[i].x, game[i].y, 100, 145)
             else:
-                window.blit(cards[0], ((i) * 150 + 380, 100))
-                game[i].x = (i) * 150 + 380
-                game[i].y = 100
-                game[i].hitBox = pygame.Rect(game[i].x, game[i].y, 100, 145)
-        elif i < 13:
-            if game[i].show:
-                window.blit(game[i].image, ((i-6)*150+300, 300))
-            else:
-                window.blit(cards[0], ((i - 6) * 150 + 300, 300))
-                game[i].x = (i-6)*150+300
-                game[i].y = 300
-                game[i].hitBox = pygame.Rect(game[i].x, game[i].y, 100, 145)
-        elif i < 20:
-            if game[i].show:
-                window.blit(game[i].image, ((i-13)*150+300, 500))
-            else:
-                window.blit(cards[0], ((i - 13) * 150 + 300, 500))
-                game[i].x = (i - 13) * 150 + 300
-                game[i].y = 500
-                game[i].hitBox = pygame.Rect(game[i].x, game[i].y, 100, 145)
+                if game[i].show:
+                    window.blit(game[i].image, ((i-20)* 150+380, 700))
+                else:
+                    window.blit(cards[0], ((i - 20) * 150 + 380, 700))
+                    game[i].x = (i - 20) * 150 + 380
+                    game[i].y = 700
+                    game[i].hitBox = pygame.Rect(game[i].x, game[i].y, 100, 145)
+    else:
+        global font
+        window.fill((40,40,40))
+        if not play_again_button.collidepoint(pygame.mouse.get_pos()):
+            pygame.draw.rect(window, (0, 153, 0), play_again_button,0,100)
+        elif play_again_button.collidepoint(pygame.mouse.get_pos()) and not pygame.mouse.get_pressed()[0]:
+            pygame.draw.rect(window, (50, 163, 50), play_again_button, 0, 100)
         else:
-            if game[i].show:
-                window.blit(game[i].image, ((i-20)* 150+380, 700))
-            else:
-                window.blit(cards[0], ((i - 20) * 150 + 380, 700))
-                game[i].x = (i - 20) * 150 + 380
-                game[i].y = 700
-                game[i].hitBox = pygame.Rect(game[i].x, game[i].y, 100, 145)
+            pygame.draw.rect(window, (0, 200, 0), play_again_button, 0, 100)
+        window.blit(winner_text, textRect)
+
 
 window = pygame.display.set_mode((1600,1000))
 
@@ -109,7 +140,7 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONUP and not game_complete:
             pickCard()
     draw()
     pygame.display.update()
